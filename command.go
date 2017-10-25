7db2658a855b9ff5601031872541e7e029ec06b4
mark.ahn <mark.ahn@gridaenergy.com>
2017-10-26 00:33:32 +0900

    plumber interface is added to commnad

--- command.go
+++ command.go
@@ -2,13 +2,23 @@ package hexa
 
 import (
 	"context"
+	"io"
 	"log"
 	"os/exec"
 )
 
-func NewCommand(parent context.Context, name string, args ...string) StoppableOne {
+type StdPlumber interface {
+	StderrPipe() (io.ReadCloser, error)
+	StdinPipe() (io.WriteCloser, error)
+	StdoutPipe() (io.ReadCloser, error)
+}
+
+func NewCommand(parent context.Context, pipeHandler func(plumber StdPlumber), name string, args ...string) StoppableOne {
 	own_ctx := NewDContextToStoppable(parent)
 	cmd := exec.CommandContext(own_ctx, name, args...)
+
+	pipeHandler(cmd)
+
 	go func() {
 		defer own_ctx.InClose()
 		err := cmd.Run()
