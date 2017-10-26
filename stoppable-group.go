46b81719575d9655040f0190bc4fd593edba164c
mark.ahn <mark.ahn@gridaenergy.com>
2017-10-26 18:36:52 +0900

    added log message

--- stoppable-group.go
+++ stoppable-group.go
@@ -2,7 +2,7 @@ package hexa
 
 import (
 	"context"
-	"logutil"
+	"log"
 	"reflect"
 )
 
@@ -80,7 +80,7 @@ func tear_down(stoppables []StoppableOne) {
 		switch stoppable {
 		case nil:
 		default:
-			logutil.Def.Debugf("[HEXA] tear down %vth element", i)
+			log.Printf("[HEXA] tear down %vth element", i)
 			stoppable.Close()
 			<-stoppable.DoneNotify()
 		}
