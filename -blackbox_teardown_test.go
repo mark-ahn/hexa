6554b044c997adee622215d741aa90bb3eea37db
mark.ahn <mark.ahn@gridaenergy.com>
2017-10-26 18:57:29 +0900

    added tear-down unittest

---
+++ -blackbox_teardown_test.go
@@ -0,0 +1,33 @@
+package hexa_test
+
+import (
+	"."
+	"context"
+	"log"
+	"testing"
+)
+
+func Test_Teardown(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("recover from %v", r)
+		}
+	}()
+	hexa.NewStoppableSpawner(context.Background(), []func() hexa.StoppableOne{
+		func() hexa.StoppableOne {
+			ctx := hexa.NewDContextToStoppable(context.Background())
+			go func() {
+				select {
+				case <-ctx.InDoneNotify():
+					log.Printf("1th func closed")
+				}
+			}()
+			return ctx
+		},
+		func() hexa.StoppableOne {
+			panic("go go go")
+
+			return nil
+		},
+	})
+}
